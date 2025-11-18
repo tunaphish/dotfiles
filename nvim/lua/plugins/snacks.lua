349c2058b7b513a6936f462e096faf86a2214633
Tuan Pham 🐟 <tunaphishy37@gmail.com>
2025-11-18 11:23:03 -0500

    customize dashboard logo

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,27 +1,92 @@
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    explorer = {
-      -- your explorer configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-    picker = {
-      sources = {
-        explorer = {
-          -- your explorer picker configuration comes here
-          -- or leave it empty to use the default settings
-        }
-      }
-    },
-    image = {
-      resolve = function(path, src)
-         if require("obsidian.api").path_is_note(path) then
-            return require("obsidian.api").resolve_image_path(src)
-         end
-      end,
-      },
-   }
+	"folke/snacks.nvim",
+	---@type snacks.Config
+	opts = {
+		dashboard = {
+			width = 60,
+			row = nil, -- dashboard position. nil for center
+			col = nil, -- dashboard position. nil for center
+			pane_gap = 4, -- empty columns between vertical panes
+			autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
+			-- These settings are used by some built-in sections
+			preset = {
+				-- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
+				---@type fun(cmd:string, opts:table)|nil
+				pick = nil,
+				-- Used by the `keys` section to show keymaps.
+				-- Set your custom keymaps here.
+				-- When using a function, the `items` argument are the default keymaps.
+				---@type snacks.dashboard.Item[]
+				keys = {
+				{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+				{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+				{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+				{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+				{ icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+				{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+				{ icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+				{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+				-- Used by the `header` section
+				header = [[
+████████╗██╗   ██╗███╗   ██╗ █████╗ ██████╗ ██╗  ██╗██╗███████╗██╗  ██╗
+╚══██╔══╝██║   ██║████╗  ██║██╔══██╗██╔══██╗██║  ██║██║██╔════╝██║  ██║
+   ██║   ██║   ██║██╔██╗ ██║███████║██████╔╝███████║██║███████╗███████║
+   ██║   ██║   ██║██║╚██╗██║██╔══██║██╔═══╝ ██╔══██║██║╚════██║██╔══██║
+   ██║   ╚██████╔╝██║ ╚████║██║  ██║██║     ██║  ██║██║███████║██║  ██║
+   ╚═╝    ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝
+			]],
+			},
+			-- item field formatters
+			formats = {
+				icon = function(item)
+				if item.file and item.icon == "file" or item.icon == "directory" then
+					return Snacks.dashboard.icon(item.file, item.icon)
+				end
+				return { item.icon, width = 2, hl = "icon" }
+				end,
+				footer = { "%s", align = "center" },
+				header = { "%s", align = "center" },
+				file = function(item, ctx)
+				local fname = vim.fn.fnamemodify(item.file, ":~")
+				fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+				if #fname > ctx.width then
+					local dir = vim.fn.fnamemodify(fname, ":h")
+					local file = vim.fn.fnamemodify(fname, ":t")
+					if dir and file then
+					file = file:sub(-(ctx.width - #dir - 2))
+					fname = dir .. "/…" .. file
+					end
+				end
+				local dir, file = fname:match("^(.*)/(.+)$")
+				return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } } or { { fname, hl = "file" } }
+				end,
+			},
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
+			},
+		},
+		explorer = {
+			-- your explorer configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		picker = {
+			sources = {
+				explorer = {
+					-- your explorer picker configuration comes here
+					-- or leave it empty to use the default settings
+				},
+			},
+		},
+		image = {
+			resolve = function(path, src)
+				if require("obsidian.api").path_is_note(path) then
+					return require("obsidian.api").resolve_image_path(src)
+				end
+			end,
+		},
+	},
 }
-
